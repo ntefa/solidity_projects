e813faf7be0859339f97cef8912b38aa001220e6
Andrea <andrea.batino94@gmail.com>
2021-04-30 14:35:52 +0200

    Added Item contract

--- Supply_Chain/supplyChain.sol
+++ Supply_Chain/supplyChain.sol
@@ -1,39 +1,65 @@
 pragma solidity ^0.8.0;
 
-contract itemizer {
+contract Item {
+    
+    uint public price;
+    uint public index;
+    uint public paidWei;
+    Itemizer parentContract;
+    
+    constructor (Itemizer _parentContract,uint _price,uint _index) {
+        parentContract=_parentContract;
+        index=_index;
+        price=_price;
+    
+    }
+    receive () external payable {
+        require(msg.value==price, "Not fully paid");
+        require(paidWei==0,"Item already paid");
+        paidWei+=msg.value;
+        (bool success,)=address(parentContract).call{value:msg.value} (abi.encodeWithSignature("getPayment(uint256)",index));
+        require(success,'Transaction did not go through');
+    }
+    
+    //fallback() external {}
+}
+
+contract Itemizer {
     
     enum steps{created,paid,delivered}
    
     struct itemStruct {
-        itemizer.steps _step;
+        Itemizer.steps _step;
         string _identifier;
-        uint _price;
+        Item _item;
     }
     
-    mapping(uint=>itemStruct) public item;
+    mapping(uint=>itemStruct) public items;
     uint index;
-    event supplyChainStep(uint _itemIndex,uint _step);
+    event supplyChainStep(uint _itemIndex,uint _step,address itemAddress);
     
     function createItem(string memory _id, uint _price) public {
-        item[index]._identifier=_id;
-        item[index]._price=_price;
-        item[index]._step=steps.created;
+        Item item = new Item(this,_price,index);
+        items[index]._identifier=_id;
+        items[index]._item=item;
+        items[index]._step=steps.created;
         index++;
-        emit supplyChainStep(index, uint(item[index]._step));
+        emit supplyChainStep(index, uint(items[index]._step),address(item));
     }
     
     function getPayment(uint _index) public payable {
-        require(msg.value>=item[_index]._price,"Item not fully paid");
-        require(item[_index]._step==steps.created,"Wrong supply chain phase");
-        item[_index]._step=steps.paid;
-        emit supplyChainStep(_index, uint(item[_index]._step));
+        Item item= items[_index]._item;
+        require(msg.sender==address(item),"Step upgradable only by Item contract");
+        require(item.price()==msg.value,"Not fully paid");
+        items[_index]._step=steps.paid;
+        emit supplyChainStep(_index, uint(items[_index]._step),address(item));
 
     }
     
     function triggerDelivery(uint _index) public {
-        require(item[_index]._step==steps.paid,"Wrong supply chain phase");
-        item[_index]._step=steps.delivered;
-        emit supplyChainStep(_index, uint(item[_index]._step));
+        require(items[_index]._step==steps.paid,"Wrong supply chain phase");
+        items[_index]._step=steps.delivered;
+        emit supplyChainStep(_index, uint(items[_index]._step),address(items[_index]._item));
 
     }
 }
