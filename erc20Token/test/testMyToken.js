e58c7b7927772f545937fe312b4a513130dcac95
ntefa <andrea.batino94@gmail.com>
2021-05-09 23:13:42 +0200

    added beforeEach to test

--- erc20Token/test/testMyToken.js
+++ erc20Token/test/testMyToken.js
@@ -1,6 +1,8 @@
-const Token = artifacts.require("MyToken"); var chai = require("chai");
+const Token = artifacts.require("MyToken"); 
+var chai = require("chai");
 const BN = web3.utils.BN;
-const chaiBN = require('chai-bn')(BN); chai.use(chaiBN);
+const chaiBN = require('chai-bn')(BN); 
+chai.use(chaiBN);
 
 var chaiAsPromised = require("chai-as-promised"); chai.use(chaiAsPromised);
 
@@ -10,8 +12,12 @@ contract("Token Test", async accounts => {
 
 	const [ initialHolder, recipient, anotherAccount ] = accounts;
 
+	beforeEach (async () => {
+		this.myToken = await Token.new(1000000000);
+	});
+
 	it("All tokens should be in my account", async () => { 
-		let instance = await Token.deployed();
+		let instance = this.myToken;
 		let totalSupply = await instance.totalSupply();
 		//old style:
 		//let balance = await instance.balanceOf.call(initialHolder); //assert.equal(balance.valueOf(), 0, "Account 1 has a balance");
@@ -20,7 +26,7 @@ contract("Token Test", async accounts => {
 	});
 
 	it("Should transfer tokens to other account", async () => { 
-		let instance = await Token.deployed();
+		let instance = this.myToken;
 		let totalSupply = await instance.totalSupply();
 		const sentTokens = 1;
 		expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply); 
@@ -31,9 +37,9 @@ contract("Token Test", async accounts => {
 	})
 
 	it('Should not allow transfers greater than the amount held in initialHolder account', async () => {
-		let instance = await Token.deployed();
+		let instance = this.myToken;
 		let balanceofAccount = await instance.balanceOf(initialHolder);
-		expect(instance.transfer(recipient,new BN (balanceofAccount+1))).to.eventually.be.rejected;
+		expect(instance.transfer(recipient, new BN (balanceofAccount+1))).to.eventually.be.rejected;
 		expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceofAccount); 
 
 	})
