7caaf679a80a3a7251b22d505fb8950a3a3a0169
ntefa <andrea.batino94@gmail.com>
2021-05-09 11:25:33 +0200

    bug fix

--- erc20Token/migrations/2_deploy_contracts.js
+++ erc20Token/migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 var myToken = artifacts.require("./myToken.sol");
 var myTokenSale = artifacts.require("./myTokenSale.sol");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
   let addr = await web3.eth.getAccounts();
-  deployer.deploy(myToken,1000000000);
-  deployer.deploy(myTokenSale,1, addr[0], myToken.address);
+  await deployer.deploy(myToken,1000000000);
+  await deployer.deploy(myTokenSale,1, addr[0], myToken.address);
   let instance = await myToken.deployed();
-  let myToken.transfer(myTokenSale.address, balanceOf(addr[0]));
+  await instance.transfer(myTokenSale.address, 1000000000);
 
 };
