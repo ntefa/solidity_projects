5f054ba274e57d3185095f69702a6f41bbe7c98b
ntefa <andrea.batino94@gmail.com>
2021-05-16 12:12:23 +0200

    prevalidate purchase function overridden

--- erc20Token/contracts/myTokenSale.sol
+++ erc20Token/contracts/myTokenSale.sol
@@ -1,16 +1,26 @@
 pragma solidity ^0.8.0;
 import "./crowdsale.sol";
+import "./kycContract.sol";
 
 contract myTokenSale is Crowdsale {
+
+	kycContract kyc;
 
     constructor(
         uint256 rate,    // rate in TKNbits
         address payable wallet,
-        IERC20 token
+        IERC20 token,
+        kycContract _kyc
     )
         Crowdsale(rate, wallet, token)
         public
     {
+    	kyc=_kyc;
 
     }
+
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view override {
+    	super._preValidatePurchase(beneficiary,weiAmount);
+		require(kyc.kycCompleted(beneficiary), "KYC not completed yet, aborting");    
+	}
 }
